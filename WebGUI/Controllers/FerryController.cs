8c0bfb966de85ebc2b8c755c233485c0751f4245
Mathias Sode-Carlsen <eaamscar@students.eaaa.dk>
2024-05-15 15:10:59 +0200

    oprettelse af webapp

---
+++ WebGUI/Controllers/FerryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebGUI.Controllers
+{
+    public class FerryController : Controller
+    {
+        // GET: Ferry
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
